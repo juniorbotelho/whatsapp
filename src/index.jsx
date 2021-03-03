ace6fc9c3625d627ddf00d78b93331e572326ed9
juniorbotelho <junowerther@gmail.com>
2021-03-03 19:51:07 -0300

    refact: organize imports from styles to better handler

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,10 @@
 import './style/global.scss'
+
 import './style/components/header.scss'
-import './style/pages/home.scss';
+
+import './style/pages/home.scss'
+
+import './style/layout/home.layout.scss'
 
 import React from 'react';
 import ReactDOM from 'react-dom';
