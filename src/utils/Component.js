7c28dc744657355ffa3d050396ccca59310118d1
juniorbotelho <junowerther@gmail.com>
2021-03-03 19:52:02 -0300

    utils: add 'Component' class to handle components from key

---
+++ src/utils/Component.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class Component extends React.Component {
+  static getInstance(children) {
+    return new Component(children);
+  }
+
+  // Component
+  constructor(children) {
+    super();
+    this.children = children;
+  }
+
+  // Public
+  getComponentByKey(key) {
+    return React.Children.map(this.children, (child) => {
+      if (key === child.key) {
+        return child;
+      }
+    });
+  }
+}
+
+export default Component;
