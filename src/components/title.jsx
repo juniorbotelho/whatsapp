83e38f64fc168a872ce2711741adc4ecb45968e0
juniorbotelho <junowerther@gmail.com>
2021-03-04 18:52:25 -0300

    chore: create var to handle classeName into components

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -1,9 +1,13 @@
-const Title = ({children}) => (
-  <h1 className="component_title mb_middle">
-    <p className="title">
-      {children}
-    </p>
-  </h1>
-)
+const Title = ({children, type}) => {
+  const title = `component_title mb_middle ${type}`
+
+  return (
+    <h1 className={title}>
+      <p className={title + 'title'}>
+        {children}
+      </p>
+    </h1>
+  );
+}
 
 export default Title
